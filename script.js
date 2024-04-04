8982d037c81093a021a6927fbe0fd06b77a6c425
WesleybGuedes <wesleyguedes2013@gmail.com>
2024-04-03 22:55:33 -0300

    feat: createCalendarDays

--- script.js
+++ script.js
@@ -17,5 +17,25 @@ createDaysOfTheWeek();
 const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
   14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
-// Escreva seu código abaixo.
+const createCalendarDays = () => {
+  const daysList = document.querySelector('#days');
 
+  for (let index = 0; index < decemberDaysList.length; index += 1) {
+    const day = decemberDaysList[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = day;
+    dayListItem.className = 'day';
+
+    if (day === 24 || day === 25 || day === 31) {
+      dayListItem.classList.add('holiday');
+    }
+
+    if (day === 4 || day === 11 || day === 18 || day === 25) {
+      dayListItem.classList.add('friday');
+    }
+
+    daysList.appendChild(dayListItem);
+  };
+}
+
+createCalendarDays();
